07a47373d2e8bf7f9f31e9e35fc474947f916e07
DungSaga <dung.saga+github@gmail.com>
2014-11-19 21:02:07 +0700

    Merge branch 'master' into hls-color-tweak

--- colorcoder.py
+++ colorcoder.py
@@ -54,7 +54,9 @@ hasher = crc8()
 scopes = []
 hex_pattern = re.compile('0x[0-9a-f]{1,2}')
 
-class colorcoder(sublime_plugin.TextCommand,sublime_plugin.EventListener):
+use_textcommand = True
+
+class colorcoder(sublime_plugin.EventListener):
 
     def on_new(self,view):
         view.settings().set('colorcode',True)
@@ -73,8 +75,9 @@ class colorcoder(sublime_plugin.TextCommand,sublime_plugin.EventListener):
                 view.settings().set('colorcode',False)
                 return
 
-        if view.size() > set.get('max_size',10000) and not view.settings().get('forcecolorcode',False):
+        if view.size() > set.get('max_size') and not view.settings().get('forcecolorcode',False):
             sublime.status_message("File is too big, disabling colorcoding as it might hurt perfromance")
+            colorcoder.remove_colorcode(view)
             view.settings().set('colorcode',False)
             return
 
@@ -94,21 +97,29 @@ class colorcoder(sublime_plugin.TextCommand,sublime_plugin.EventListener):
         self.on_load(view)
 
     def on_modified_async(self, view):
-        view.run_command("colorcoder")
+        global use_textcommand
+        if view.settings().get('colorcode',False) or view.settings().get('forcecolorcode',False):
+            if use_textcommand:
+                view.run_command("colorcode")
+            else:
+                colorcoder.colorcode(view)
 
     @staticmethod
     def update_scopes():
         global scopes
         scopes = sublime.load_settings("colorcoder.sublime-settings").get('scopes')
 
+    @staticmethod
+    def update_use_textcommand():
+        global use_textcommand
+        use_textcommand = sublime.load_settings("colorcoder.sublime-settings").get('use_fast_highlighting_but_undo_typing_letterwise')
+
     def on_post_text_command(self, view, cmd, args):
         if cmd=="set_file_type":
-            sublime.active_window().active_view().run_command("colorcoder")
+            self.on_modified_async(sublime.active_window().active_view())
 
-    def __init__(self, view = None):
-        self.view = view
-
-    def run(self, edit):
+    @staticmethod
+    def colorcode(view):
         global hasher, scopes
 
         regs = {}
@@ -119,13 +130,23 @@ class colorcoder(sublime_plugin.TextCommand,sublime_plugin.EventListener):
         crc_in_text = set.get('crc_in_text', False)
         hash_fn = hasher.forced_crc if crc_in_text else hasher.crc
         for sel in scopes:
-            for r in self.view.find_by_selector(sel):
-                regs[hex(hash_fn(self.view.substr(r)))].append(r)
+            for r in view.find_by_selector(sel):
+                regs[hex(hash_fn(view.substr(r)))].append(r)
 
         for key in regs:
-            self.view.add_regions('cc'+key,regs[key],'cc'+key,'', sublime.DRAW_NO_OUTLINE )
+            view.add_regions('cc'+key,regs[key],'cc'+key,'', sublime.DRAW_NO_OUTLINE )
 
         del regs
+
+    @staticmethod
+    def remove_colorcode(view):
+        for i in map(hex,range(256)):
+            view.erase_regions('cc'+i)
+
+class colorcode(sublime_plugin.TextCommand):
+    def run(self, edit):
+        colorcoder.colorcode(self.view)
+
 
 class colorcodertoggler(sublime_plugin.ApplicationCommand):
     def run(self):
@@ -135,18 +156,18 @@ class colorcodertoggler(sublime_plugin.ApplicationCommand):
         view.settings().set('forcecolorcode',False)
 
         if cc:
-            for i in map(hex,range(256)):
-                view.erase_regions('cc'+i)
+            colorcoder.remove_colorcode(view)
         else:
-            if view.size() > sublime.load_settings("colorcoder.sublime-settings").get('max_size',10000):
+            if view.size() > sublime.load_settings("colorcoder.sublime-settings").get('max_size'):
                 view.settings().set('forcecolorcode',True)
-            view.run_command("colorcoder")
+            view.run_command("colorcode")
 
     def is_checked(self):
-        return sublime.active_window().active_view().settings().get('colorcode',False)
+        viewset = sublime.active_window().active_view().settings()
+        return viewset.get('colorcode',False) or viewset.get('forcecolorcode',False)
 
     def description(self):
-        if sublime.active_window().active_view().size() > sublime.load_settings("colorcoder.sublime-settings").get('max_size',10000):
+        if sublime.active_window().active_view().size() > sublime.load_settings("colorcoder.sublime-settings").get('max_size'):
             return "Colorcoding may hurt performance, File is large"
         else:
             return "Colorcode this view"
@@ -176,7 +197,7 @@ class colorshemeemodifier(sublime_plugin.ApplicationCommand):
         read_original = read_original and sublime.load_settings("Preferences.sublime-settings").has("original_color_scheme")
         if read_original and sublime.load_settings("Preferences.sublime-settings").get('color_scheme').find('/Colorcoder/') == -1:
             read_original = False
-        if sublime.load_settings("Preferences.sublime-settings").get('original_color_scheme').find('/Colorcoder/') != -1:
+        if read_original and sublime.load_settings("Preferences.sublime-settings").get('original_color_scheme').find('/Colorcoder/') != -1:
             print("original theme already colorcoded, abort")
             return
         global modification_running
@@ -244,7 +265,9 @@ class colorcoderInspectScope(sublime_plugin.ApplicationCommand):
 def plugin_loaded():
     sublime.load_settings("Preferences.sublime-settings").add_on_change('color_scheme',colorshemeemodifier.maybefixscheme)
     sublime.load_settings("colorcoder.sublime-settings").add_on_change('scopes',colorcoder.update_scopes)
+    sublime.load_settings("colorcoder.sublime-settings").add_on_change('use_textcommand',colorcoder.update_use_textcommand)
     colorcoder.update_scopes()
+    colorcoder.update_use_textcommand()
     pp = sublime.packages_path()
     if not os.path.exists(pp+"/Colorcoder"):
         os.makedirs(pp+"/Colorcoder")
@@ -257,5 +280,4 @@ def plugin_loaded():
     for wnd in sublime.windows():
         for view in wnd.views():
             view.settings().set('colorcode',True)
-            view.run_command("colorcoder")
-
+            view.run_command("colorcode")
